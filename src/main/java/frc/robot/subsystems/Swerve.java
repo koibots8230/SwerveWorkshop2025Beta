c581c5cf9e0386e853bdc58f91af0ab5c8532057
Erik Skiles <skipethstopholes@gmail.com>
2024-12-12 18:29:09 -0500

    Cleaned up units in driveFieldRelative

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,8 +1,6 @@
 package frc.robot.subsystems;
 
 // STEP 6: import static Meters and Radians
-import static edu.wpi.first.units.Units.Meters;
-import static edu.wpi.first.units.Units.Radians;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -13,6 +11,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Constants.SwerveConstants;
 import java.util.function.DoubleSupplier;
 
 // STEP 2: Add the epilogue @Logged annotation
@@ -44,16 +43,13 @@ public class Swerve extends SubsystemBase {
     // STEP 6: Update the estimated pose using the requested field-relative velocities
     // The process of converting AngularVelocity into a Rotation2d is comp
 
-    var theta = omega.times(Constants.SwerveConstants.swerveUpdatePeriod);
-    var rads = theta.in(Radians);
-    var changeInRotation = new Rotation2d(rads);
-    Rotation2d newRotation = estimatedPose.getRotation().plus(changeInRotation);
-
     estimatedPose =
         new Pose2d(
-            estimatedPose.getX() + x.times(Constants.SwerveConstants.swerveUpdatePeriod).in(Meters),
-            estimatedPose.getY() + y.times(Constants.SwerveConstants.swerveUpdatePeriod).in(Meters),
-            newRotation);
+            x.times(SwerveConstants.swerveUpdatePeriod).plus(estimatedPose.getMeasureX()),
+            y.times(SwerveConstants.swerveUpdatePeriod).plus(estimatedPose.getMeasureY()),
+            estimatedPose
+                .getRotation()
+                .plus(new Rotation2d(omega.times(SwerveConstants.swerveUpdatePeriod))));
   }
 
   /**
@@ -74,19 +70,6 @@ public class Swerve extends SubsystemBase {
                 Constants.SwerveConstants.MAX_SPEED.times(-x.getAsDouble()),
                 Constants.SwerveConstants.MAX_SPEED.times(-y.getAsDouble()),
                 Constants.SwerveConstants.MAX_ANGULAR_VELOCITY.times(-omega.getAsDouble())),
-        this);
-  }
-
-  public Command driveFieldRelativeCommand(double x, double y, double omega) {
-
-    // STEP 6: Convert the relative Joystick inputs to Velocities by multiplying the input by the
-    // MAX Speeds
-    return Commands.run(
-        () ->
-            this.driveFieldRelative(
-                Constants.SwerveConstants.MAX_SPEED.times(-x),
-                Constants.SwerveConstants.MAX_SPEED.times(-y),
-                Constants.SwerveConstants.MAX_ANGULAR_VELOCITY.times(-omega)),
         this);
   }
 }
